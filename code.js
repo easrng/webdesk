c1ee23046fe0f65922087bf66fcecca2e8d850a7
easrng <easrng@gmail.com>
2019-03-06 13:36:54 -0500

    Update code.js

--- code.js
+++ code.js
@@ -1,14 +1,10 @@
-window._winId = 0;
-
 function newWindow(title, url) {
-    window._winId++;
-    document.body.innerHTML += "<div id=\"win-" + _winId + "\" class=\"win\" title=\"" + title + "\"><div class=\"title\">" + title + "</div><iframe src=\"" + url + "\"></iframe></div>";
-    $(function() {
-        $("#win-" + _winId).draggable({
-                stack: "body .win",
-                containment: $('body'),
-                cursor: "move"
-            });
-        $("#win-" + _winId).resizable();
-    });
+    var win=$("<div class=\"win\" title=\"" + title + "\"><div class=\"title\">" + title + "</div><iframe src=\"" + url + "\"></iframe></div>";
+    win.appendTo($("body"))
+    win.draggable({
+            stack: "body .win",
+            containment: $('body'),
+            cursor: "move"
+        });
+        win.resizable();
 }
