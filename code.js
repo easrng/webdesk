ef1d5d8cca1dd408a751870be57e5c231f1a9125
easrng <easrng@gmail.com>
2019-03-06 13:41:07 -0500

    Update code.js

--- code.js
+++ code.js
@@ -1,10 +1,12 @@
+var windows=[];
 function newWindow(title, url) {
-    var win=$("<div class=\"win\" title=\"" + title + "\"><div class=\"title\">" + title + "</div><iframe src=\"" + url + "\"></iframe></div>";
-    win.appendTo($("body"))
-    win.draggable({
+    windows.push($("<div class=\"win\" title=\"" + title + "\"><div class=\"title\">" + title + "</div><iframe src=\"" + url + "\"></iframe></div>");
+    var currentid=windows.length;
+    windows[currentid].appendTo($("body"));
+    windows[currentid].draggable({
             stack: "body .win",
             containment: $('body'),
             cursor: "move"
         });
-        win.resizable();
+        windows[currentid].resizable();
 }
