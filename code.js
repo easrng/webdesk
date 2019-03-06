c799e5a4b9986c3a98934b7b226646d1738cb115
easrng <easrng@gmail.com>
2019-03-06 15:47:35 -0500

    Update code.js

--- code.js
+++ code.js
@@ -1,7 +1,7 @@
 var windows=[];
 function newWindow(title, url) {
     var currentid=windows.length;
-    windows.push($("<div class=\"win\" title=\"" + title + "\"><div class=\"title\">" + title + '<div class=\"buttons\"><button class="mdl-button mdl-js-button mdl-button--icon"><i class="material-icons">minimize</i></button><button class="mdl-button mdl-js-button mdl-button--icon"><i class="material-icons">crop_square</i></button><button class="mdl-button mdl-js-button mdl-button--icon"><i class="material-icons">close</i></button></div></div><iframe src=\"' + url + "\"></iframe></div>"));
+    windows.push($("<div class=\"win\" title=\"" + title + "\"><div class=\"title\"><div class=\"title-text\">" + title + '</div><div class=\"buttons\"><button class="mdl-button mdl-js-button mdl-button--icon"><i class="material-icons">minimize</i></button><button class="mdl-button mdl-js-button mdl-button--icon"><i class="material-icons">crop_square</i></button><button class="mdl-button mdl-js-button mdl-button--icon"><i class="material-icons">close</i></button></div></div><iframe src=\"' + url + "\"></iframe></div>"));
     windows[currentid].appendTo($("body"));
     windows[currentid].draggable({
             stack: "body .win",
