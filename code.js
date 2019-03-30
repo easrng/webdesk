2e89d2d6968722247115fee269ccd112850ad8cf
easrng <easrng@gmail.com>
2019-03-30 17:46:39 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,8 +1,8 @@
 var windows=[];
 var numwins=0;
 function newWindow(title, url) {
-    numwins++
     var currentid=numwins;
+    numwins++;
     windows[currentid]=($("<div data-win=\""+currentid+"\" class=\"win\" title=\"" + title + "\"><div class=\"title\"><div class=\"title-text\">" + title + '</div><div class=\"buttons\"><button class="min mdl-button mdl-js-button mdl-button--icon"><i class="material-icons">minimize</i></button><button class="mdl-button mdl-js-button mdl-button--icon max"><i class="material-icons">crop_square</i></button><button class="mdl-button mdl-js-button mdl-button--icon close"><i class="material-icons">close</i></button></div></div><iframe src=\"' + url + "\"></iframe></div>"));
     windows[currentid].appendTo($("body"));
     windows[currentid].draggable({
